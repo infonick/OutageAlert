2ca9c251d5ca1ddbd6f8dd25a00a155cd67e9d70
Infonick <49381882+infonick@users.noreply.github.com>
2021-02-08 10:48:24 -0800

    Divide RetrieveJSON file into two parts based on functionality.

--- AppServer/AppModelRetrieveJSON.py
+++ AppServer/AppModelRetrieveJSON.py
@@ -66,21 +66,22 @@ if r.headers.get('content-type') != 'application/json':
 
 
 # Handle the retrieved JSON file ----------------------------------------------------------------------
+# Record time of JSON file retrieval
 try:
     # Get the time of the JSON file retrieval as a Python DateTime object
-    currentTime = OATime.GetTimeFromURLHeader(r)
+    currentTime = AppTimeLib.GetTimeFromURLHeader(r)
 
 except:
-    currentTime = OATime.GetCurrentUTCTime() # Get the current time in UTC from the system if getting the time from the HTTP header is unsuccessful
+    currentTime = AppTimeLib.GetCurrentUTCTime() # Get the current time in UTC from the system if getting the time from the HTTP header is unsuccessful
     # Program a routine that notifies the admin that retrieving the time from the HTTP header did not work                  <----
 
 
-#convert JSON file to a list of Python dictionaries:
+# Convert JSON file to a list of Python dictionaries:
 try:
     outages = r.json()
 
 except: 
-    #Error in parsing the json file
+    # Error in parsing the json file
     for line in sys.exc_info():
         print(line)
     # Program a routine that notifies the admin, etc.                                                                       <----
@@ -96,10 +97,10 @@ for outage in outages:
     outageIDList.append(outage['id'])
 
 # Retrieve existing outages from the database
-(dbOutages, err) = AppModel.GetOutageList(outageIDList)
+(dbOutages, err) = AppModelDB.GetOutageList(outageIDList)
 
 if err != None:
-    #There was a problem retrieving the existing outages from the database. Handle this error                               <----
+    # There was a problem retrieving the existing outages from the database. Handle this error                               <----
     print("ERROR RETRIEVING EXISTING OUTAGES FROM DATABASE!")
 
 else:
@@ -122,7 +123,7 @@ else:
 # NEW POWER OUTAGES ---------------------------------------------------------------------
 # Save new outage data to the database 
 
-err = AppModel.SaveNewOutages(newOutages)
+err = AppModelDB.SaveNewOutages(newOutages)
 
 if err != None:
     #There was a problem saving the new outages to the database. Handle this error                                          <----
@@ -132,7 +133,7 @@ if err != None:
 
 
 # Alert users of new outages                                                                                                <----
-# NEED TO COMPLETE THIS SECTION!    
+# NEED TO SEND newOutages TO THE CODE THAT HANDLES THIS IN AppModelUserContact                                              <----   
 
 
 
@@ -178,7 +179,7 @@ for existingOutage in existingOutages:
 # Update database with new outage information
 dbUpdates = [outage for (_,_,outage) in updateOutages] #python list comprehension
 
-err = AppModel.UpdateOutage(dbUpdates)
+err = AppModelDB.UpdateOutage(dbUpdates)
 
 if err != None:
     #There was a problem saving the updated outages to the database. Handle this error                                      <----
@@ -187,121 +188,7 @@ if err != None:
 
 
 
-# Figure out what changed with each outage so we can act on those changes
-for i in range(len(updateOutages)):
-    (_, dbOutageInfo, updatedOutageInfo) = updateOutages[i] # Get the two outage dictionaries
-    
-    for key in updatedOutageInfo:                           # For each key in the dictionary...
-        if updatedOutageInfo[key] == dbOutageInfo[key]:     # If the keys are the same...
-            updateOutages[i].pop(key)                       # Remove the key & value from these dictionaries
-            dbOutageInfo[i].pop(key)
-            break
-    if len(updatedOutageInfo) == len(dbOutageInfo) == 0:    # If an outage record has no keys left, remove it from the update list
-        updateOutages.pop(i)
-
-
-    
-#Alert users to outage updates
-
-updateAlerts = [] # This is a ist of tuples consisting of the outage ID number, and all the related update messages for that outage ID
-
-for i in range(len(updateOutages)):
-    (outageID, dbOutageInfo, updatedOutageInfo) = updateOutages[i]
-
-    outageMessages = [] # This is a list of strings for the current outage ID number. Each string is a message to the user about a change in this specific power outage.
-
-    for key in updatedOutageInfo:
-        oldValue = dbOutageInfo[key]
-        newValue = updatedOutageInfo[key]
-
-        # Generate generic power outage update messages for users
-
-        if key == 'cause':
-            if oldValue == None:
-                outageMessages.append("Cause of power outage: " + newValue)
-            else:
-                outageMessages.append("Cause of power outage updated from \'" + oldValue + "\' to \'" + newValue + "\'.")
-            break
-
-
-        elif key == 'crewStatusDescription':
-            if oldValue == None:
-                outageMessages.append("Power restoration crew status: " + newValue)
-            else:
-                outageMessages.append("Power restoration crew status updated from \'" + oldValue + "\' to \'" + newValue + "\'.")
-            break
-
-
-        elif key == 'crewEta':
-            dateTimeCrewETA = OATime.DateTimeFromJSToPython(newValue)
-            dateTimeCrewETA = OATime.PythonChangeTimeZone(dateTimeCrewETA, 'America/Vancouver')
-            if oldValue == None:
-                outageMessages.append("Power restoration crew ETA: " + datetime.datetime.strftime(dateTimeOff, '%Y-%b-%d %I:%M:%S %p %Z'))
-            else:
-                outageMessages.append("Power restoration crew ETA updated to: " + datetime.datetime.strftime(dateTimeOff, '%Y-%b-%d %I:%M:%S %p %Z'))
-            break
-
-
-        elif key == 'dateOff':
-            dateTimeOff = OATime.DateTimeFromJSToPython(newValue)
-            dateTimeOff = OATime.PythonChangeTimeZone(dateTimeOff, 'America/Vancouver')
-            if oldValue == None:
-                outageMessages.append("Power outage began on " + datetime.datetime.strftime(dateTimeOff, '%Y-%b-%d %I:%M:%S %p %Z'))
-            else:
-                outageMessages.append("Power outage start time was updated to " + datetime.datetime.strftime(dateTimeOff, '%Y-%b-%d %I:%M:%S %p %Z'))
-            break
-
-
-        elif key == 'dateOn':
-            dateTimeOn = OATime.DateTimeFromJSToPython(newValue)
-            dateTimeOn = OATime.PythonChangeTimeZone(dateTimeOn, 'America/Vancouver')
-            if oldValue == None:
-                outageMessages.append("Power restored on " + datetime.datetime.strftime(dateTimeOn, '%Y-%b-%d %I:%M:%S %p %Z'))
-            else:
-                outageMessages.append("Power restoration time was updated to " + datetime.datetime.strftime(dateTimeOn, '%Y-%b-%d %I:%M:%S %p %Z'))
-            break
-
-
-        # OTHER DICTIONARY 'KEY' OPTIONS, saved in case we want to use them.
-        # elif key == 'polygon':
-        #     # need to check for newly affected customers, or newly restored customers if the polygon changes                 <----
-        #     break
-        # elif key == 'lastUpdated':
-        #     # What do we do with this part? anything?                                                                        <----
-        #     break
-        # elif key == 'numCustomersOut':
-        #     break
-        # elif key == 'id':
-        #     break
-        # elif key == 'gisId':
-        #     break
-        # elif key == 'regionId':
-        #     break
-        # elif key == 'municipality':
-        #     break
-        # elif key == 'area':
-        #     break
-        # elif key == 'regionName':
-        #     break
-        # elif key == 'crewEtr':
-        #     break
-        # elif key == 'showEta':
-        #     break
-        # elif key == 'showEtr':
-        #     break
-        # elif key == 'latitude':
-        #     break
-        # elif key == 'longitude':
-        #     break
-    
-    updateAlerts.append((outageID, outageMessages)) # Create a tuple consisting of the outage ID number, and all the related update messages for that outage ID
-
-
-#send the updateAlerts tuple-list to a function that sends the messages to our users                                        <----
-
-
-
-
+# NEED TO SEND updateOutages TO THE CODE THAT HANDLES THIS IN AppModelUserContact                                           <----
 
 
 
